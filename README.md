4466bb71944a5f439e9639481c9c253d567327ad
root <root@promc-2n-dhcp105-33.eng.vmware.com>
2016-11-15 09:13:50 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vcs-proto1
---
+++ java/api-mock/src/main/java/com/vmware/photon/controller/api/model/VmCreateSpec.java
@@ -0,0 +1,44 @@
+package com.vmware.photon.controller.api.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class VmCreateSpec {
+
+	public void setName(String vmName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setFlavor(String vmFlavorName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setTags(Set<String> vmTags) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setSourceImageId(String imageId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setSubnets(List<String> networks) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAttachedDisks(List<AttachedDiskCreateSpec> attachedDisks) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEnvironment(Map<String, String> environment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
